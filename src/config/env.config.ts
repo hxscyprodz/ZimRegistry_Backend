3be8f12c2c8ea850cedf864efe444158458dbc96
hxscyprodz <hxscyprodz@gmail.com>
2026-04-29 23:04:32 +0200

    Add the .env config

---
+++ src/config/env.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config({ path: path.join(__dirname, '../../.env')});
+
+const mandatoryVariables = [
+    "PORT",
+    "APP_ENV",
+];
+
+const missingVariables = mandatoryVariables.filter((variable) => !process.env[variable]);
+
+if(missingVariables.length > 0) {
+    const variableString = JSON.stringify(missingVariables);
+    console.log(`Environment variable(s) ${variableString.substring(1, variableString.length - 1)} are required to start the server.
+    
+    If running on local machine create a .env file in your route directory and define them there.
+    `);
+
+    process.exit(1);
+    
+};
+
+
+const config = {
+    PORT: process.env.PORT,
+    APP_ENV: process.env.APP_ENV
+};
+
+export default config;
