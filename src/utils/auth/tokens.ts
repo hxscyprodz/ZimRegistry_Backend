17e0d83b6c6d60fa512ef6c4dea4ca5c83cea9ba
hxscyprodz <hxscyprodz@gmail.com>
2026-05-01 11:32:35 +0200

    Add authentication middleware

--- src/utils/auth/tokens.ts
+++ src/utils/auth/tokens.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 import config from "../../config/env.config";
-import { ERole, ITokenPayload } from "../../types/types";
+import { ITokenPayload } from "../../types/types";
 
 const generateAccessToken = async(payload: ITokenPayload) => {
     return await jwt.sign(payload, config.JWT_ACCESS_TOKEN || "", { expiresIn: "15m"});
@@ -10,9 +10,20 @@ const generateRefreshToken = async(payload: ITokenPayload) => {
     return await jwt.sign(payload, config.JWT_REFRESH_TOKEN || "", { expiresIn: "7d" });
 };
 
+const verifyAccessToken = async(token: string) => {
+    return await jwt.verify(token, config.JWT_ACCESS_TOKEN || "");
+};
+
+const verifyRefreshToken = async(token: string) => {
+    return await jwt.verify(token, config.JWT_REFRESH_TOKEN || "");
+};
+
+
 const tokenUTILS = {
     generateAccessToken,
     generateRefreshToken,
+    verifyAccessToken,
+    verifyRefreshToken,
 };
 
 export default tokenUTILS;
