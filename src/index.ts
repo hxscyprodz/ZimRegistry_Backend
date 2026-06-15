9b201b133430509331ae0ff7cec3977404d0324f
hxscyprodz <hxscyprodz@gmail.com>
2026-06-15 16:50:41 +0200

    Add the webhook verification

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express, { Request, Response, Application } from 'express';
 import config  from './config/envConfig';
 import logger from './services/logger';
+import whatsappRoutes from './routes/whatsapp.routes';
+
 
 const app: Application = express();
 const port = config.PORT;
@@ -8,13 +10,21 @@ const port = config.PORT;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//routes
+app.use(whatsappRoutes);
 
-app.get('/', (req: Request, res: Response) => {
+//health check
+app.get('/health', (req: Request, res: Response) => {
   res.send('ZimRegistry Backend is running with TypeScript!');
 });
 
 app.listen(port, () => {
-  logger.info(`[server]: Server is running at http://localhost:${port}`);
+    try{
+        logger.info(`[server]: Server is running at http://localhost:${port}`);
+    } catch(error: any) {
+        logger.error(`Error occured: ${error.message}`);
+        process.exit(1);
+    }
 });
 
 export default app;
