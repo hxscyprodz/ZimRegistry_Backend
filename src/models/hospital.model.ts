a5f60ae21cc96d207c01dd96c26de8fd0822c04b
hxscyprodz <hxscyprodz@gmail.com>
2026-07-09 17:43:09 +0200

    Add db auto increment

--- src/models/hospital.model.ts
+++ src/models/hospital.model.ts
@@ -1,9 +1,9 @@
 import { Schema, model } from "mongoose";
 import { IHospital } from "../types/types";
+import { applyAutoIncrement } from "../utils/autoIncrement";
 
 const HospitalSchema = new Schema<IHospital>(
   {
-    hospitalId: { type: String, required: true },
     createdBy: { type: String, required: true },
     hospitalName: { type: String, required: true },
     district: { type: String, required: true },
@@ -11,8 +11,16 @@ const HospitalSchema = new Schema<IHospital>(
   },
   {
     timestamps: true,
+    toJSON: { getters: true },
+    toObject: { getters: true },
   },
 );
+
+applyAutoIncrement(HospitalSchema, {
+  idField: "hospitalId",
+  sequenceId: "hospital_seq",
+  prefix: "HT",
+});
 
 const Hospital = model<IHospital>("Hospital", HospitalSchema);
 export default Hospital;
--- src/models/province.model.ts
+++ src/models/province.model.ts
@@ -1,15 +1,25 @@
 import { Schema, model } from "mongoose";
 import { IProvince } from "../types/types";
+import { applyAutoIncrement } from "../utils/autoIncrement";
 
-const ProvinceSchema = new Schema({
-  provinceId: { type: String, required: true },
-  name: { type: String, required: true },
-  districts: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "District",
-    },
-  ],
+const ProvinceSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    districts: [{
+        name: { type: String, required: true },
+    }],
+  },
+  {
+    timestamps: true,
+    toObject: { getters: true },
+    toJSON: { getters: true },
+  },
+);
+
+applyAutoIncrement(ProvinceSchema, {
+  idField: "provinceId",
+  sequenceId: "province_seq",
+  prefix: "PR",
 });
 
 const Province = model<IProvince>("Province", ProvinceSchema);
