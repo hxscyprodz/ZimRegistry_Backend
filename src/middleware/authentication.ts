e1c189213d4c442bc2edc6b7621613a07f8afdac
hxscyprodz <hxscyprodz@gmail.com>
2026-08-03 00:31:05 +0200

    Add the basic auth setup

---
+++ src/middleware/authentication.ts
@@ -0,0 +1,53 @@
+import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { authenticationHeaderSchema } from "../validators/validators";
+import { verifyAccessToken } from "../services/jsonwebtokens";
+import logger from "../services/logger";
+import { AuthRequest } from "../types";
+
+const FLAG = "AUTHENTICATION";
+
+export const authenticationMiddleware = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const isHeaderValid = authenticationHeaderSchema.safeParse(req.headers);
+    if (!isHeaderValid.success) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        status: false,
+        message: "Token provided is invalid",
+        data: null,
+      });
+    }
+    const token = isHeaderValid.data.authorization.split(" ")[1];
+    if (!token) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        message: "Invalid token format. Expected 'Bearer <token>'.",
+        data: null,
+      });
+    }
+    const payload = await verifyAccessToken(token);
+    if (!payload) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        message: "Token provided is invalid",
+        data: null,
+      });
+    }
+
+    return (req.user = payload);
+    next();
+  } catch (error: any) {
+    logger.error(
+      `[ ${FLAG} ] - An error occurred while authenticating user: ${error.message}`,
+    );
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      status: false,
+      message: "An error occurred while authenticating user",
+      data: null,
+    });
+  }
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,1 +1,12 @@
+import { Request } from "express";
+
+export type TRoles = "super-admin" | "admin" | "user";
+
 export type TBirthApplication = "BC" | "ID";
+export interface ITokenPayload {
+    userId: string,
+    role: TRoles,
+}
+export interface AuthRequest extends Request {
+    user?: ITokenPayload;
+}
