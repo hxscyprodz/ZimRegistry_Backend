836abecd2c5988be614a5ddf157efcb42ee69fec
hxscyprodz <hxscyprodz@gmail.com>
2026-08-12 01:15:41 +0200

    Add the auth profile route

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,13 +2,14 @@ import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { loginSchema, registerSchema } from "../validators/validators";
 import { hashPassword, comparePassword } from "../services/bcrypt";
-import { users } from "../db/schemas";
+import { staffMember, users } from "../db/schemas";
 import { birthCertificates } from "../db/schemas";
 import { db } from "../config/db";
 import CustomError from "../utils/CustomError";
 import logger from "../services/logger";
 import { eq } from "drizzle-orm";
 import { generateAccessToken } from "../services/jsonwebtokens";
+import { AuthRequest } from "../types";
 
 export const login = async (req: Request, res: Response) => {
   const FLAG = "USER_LOGIN";
@@ -23,12 +24,29 @@ export const login = async (req: Request, res: Response) => {
   }
   try {
     const { email, password } = isRequestValid.data;
-    const user = await db
-      .select()
+    const [user] = await db
+      .select({
+        users: {
+          id: users.id,
+          firstName: users.firstName,
+          surname: users.surname,
+          email: users.email,
+          role: users.role,
+          password: users.password,
+        },
+        staffMember: {
+          staffId: staffMember.staffId,
+          stationId: staffMember.stationId,
+        },
+      })
       .from(users)
       .where(eq(users.email, email))
+      .leftJoin(
+        staffMember,
+        eq(users.nationalIdNumber, staffMember.nationalIdNumber),
+      )
       .limit(1);
-    if (user.length === 0) {
+    if (!user) {
       return res.status(StatusCodes.UNAUTHORIZED).json({
         success: false,
         message: "Credentials provided are invalid",
@@ -38,12 +56,12 @@ export const login = async (req: Request, res: Response) => {
 
     const isCorrectPassword = await comparePassword(
       password,
-      user[0]?.password!,
+      user.users.password!,
     );
 
     if (!isCorrectPassword) {
       logger.warn(
-        `[ ${FLAG} ] - Login attempt for user : ${user[0]?.id} failed to wrong password`,
+        `[ ${FLAG} ] - Login attempt for user : ${user?.users.id} failed to wrong password`,
       );
 
       return res.status(StatusCodes.UNAUTHORIZED).json({
@@ -54,20 +72,25 @@ export const login = async (req: Request, res: Response) => {
     }
 
     const token = generateAccessToken({
-      userId: user[0]?.id!,
-      role: user[0]?.role!,
+      userId: user.users.id!,
+      role: user.users.role!,
     });
 
     logger.info(
-      `[ ${FLAG} ] - Login attempt for user : ${user[0]?.id} successful`,
+      `[ ${FLAG} ] - Login attempt for user : ${user.users.id} successful`,
     );
 
     return res.status(StatusCodes.OK).json({
       success: true,
       message: "Login successful",
       data: {
-        id: user[0]?.id,
-        email: user[0]?.email,
+        id: user?.users.id,
+        email: user?.users.email,
+        name: `${user?.users.firstName} ${user?.users.surname}`,
+        role: user?.users.role,
+        stationId: user?.staffMember?.stationId && user?.staffMember?.stationId,
+        employeeNumber:
+          user?.staffMember?.staffId && user?.staffMember?.staffId,
       },
       token,
     });
@@ -75,7 +98,73 @@ export const login = async (req: Request, res: Response) => {
     logger.error(
       `[ ${FLAG} ] - An error occurred while logging in : ${error.message}`,
     );
-    throw new CustomError(error.message, StatusCodes.INTERNAL_SERVER_ERROR);
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "An error occurred while logging in",
+      data: null,
+    });
+  }
+};
+
+export const profile = async (req: AuthRequest, res: Response) => {
+  const FLAG = "USER_PROFILE";
+  try {
+    const userId = req.user?.userId;
+    if (!userId) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        message: "Unauthorized",
+        data: null,
+      });
+    }
+
+    const [user] = await db
+      .select({
+        user: {
+          role: users.role,
+          firstName: users.firstName,
+          surname: users.surname,
+        },
+        staffMember: {
+          stationId: staffMember.stationId,
+          staffId: staffMember.staffId,
+        },
+      })
+      .from(users)
+      .leftJoin(
+        staffMember,
+        eq(users.nationalIdNumber, staffMember.nationalIdNumber),
+      )
+      .where(eq(users.id, userId))
+      .limit(1);
+
+    if (!user) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        message: "Bad credentials provided",
+        data: null,
+      });
+    }
+
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      message: "Profile fetched successfully",
+      data: {
+        employeeNumber: user.staffMember?.staffId,
+        stationId: user.staffMember?.stationId,
+        role: user.user.role,
+        name: `${user.user.firstName} ${user.user.surname}`,
+      },
+    });
+  } catch (error: any) {
+    logger.error(
+      `[ ${FLAG} ] - An error occurred while fetching profile : ${error.message}`,
+    );
+    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "An error occurred while fetching profile",
+      data: null,
+    });
   }
 };
 
@@ -90,22 +179,8 @@ export const register = async (req: Request, res: Response) => {
     });
   }
   try {
-    const { email, password, confirmPassword, nationalIdNumber } =
-      isRequestValid.data;
-
-    const isNationalIdRegistered = await db
-      .select()
-      .from(birthCertificates)
-      .where(eq(birthCertificates.nationalIdNumber, nationalIdNumber))
-      .limit(1);
-
-    if (isNationalIdRegistered.length <= 0) {
-      return res.status(StatusCodes.UNAUTHORIZED).json({
-        success: false,
-        message: "National ID Number not registered",
-        data: null,
-      });
-    }
+    const { confirmPassword, ...userData } = isRequestValid.data;
+    const { nationalIdNumber, email, password } = userData;
 
     if (password !== confirmPassword) {
       return res.status(StatusCodes.BAD_REQUEST).json({
@@ -115,13 +190,29 @@ export const register = async (req: Request, res: Response) => {
       });
     }
 
-    const userExists = await db
+    const [isNationalIdRegistered] = await db
+      .select({
+        nationalIdNumber: birthCertificates.nationalIdNumber,
+      })
+      .from(birthCertificates)
+      .where(eq(birthCertificates.nationalIdNumber, nationalIdNumber))
+      .limit(1);
+
+    if (!isNationalIdRegistered) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        success: false,
+        message: "National ID Number not registered",
+        data: null,
+      });
+    }
+
+    const [userExists] = await db
       .select()
       .from(users)
       .where(eq(users.email, email))
       .limit(1);
 
-    if (userExists.length > 0) {
+    if (userExists) {
       return res.status(StatusCodes.CONFLICT).json({
         success: false,
         message: "User already exists",
@@ -130,27 +221,30 @@ export const register = async (req: Request, res: Response) => {
     }
 
     const hashedPassword = await hashPassword(password);
-    isRequestValid.data.password = hashedPassword;
 
-    const user = await db.insert(users).values(isRequestValid.data).returning({
-      id: users.id,
-      role: users.role,
-    });
+    const [user] = await db
+      .insert(users)
+      .values({
+        ...userData,
+        password: hashedPassword,
+      })
+      .returning({
+        id: users.id,
+        role: users.role,
+      });
 
     const token = generateAccessToken({
-      userId: user[0]?.id!,
-      role: user[0]?.role!,
+      userId: user?.id!,
+      role: user?.role!,
     });
 
-    logger.info(
-      `[ ${FLAG} ] - User created successfully with id: ${user[0]?.id}`,
-    );
+    logger.info(`[ ${FLAG} ] - User created successfully with id: ${user?.id}`);
 
     return res.status(StatusCodes.CREATED).json({
       success: true,
       message: "User created successfully",
       data: {
-        id: user[0]?.id,
+        id: user?.id,
       },
       token,
     });
