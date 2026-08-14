6cad6ffa178959e1e8dec69e7229747961e7af45
hxscyprodz <hxscyprodz@gmail.com>
2026-08-14 23:33:48 +0200

    Add the get birth and id applications

--- src/routes/applications/birthApp.routes.ts
+++ src/routes/applications/birthApp.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
-import { createApplication } from "../../controllers/applications/birthApp.controller";
+import {
+  createApplication,
+  getApplications,
+} from "../../controllers/applications/birthApp.controller";
 
 const router = Router();
 
 router.post("/apply", createApplication);
+router.get("/", getApplications);
 
 export default router;
