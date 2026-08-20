32b1e491ffee8789033e67442b2da81c6d523e60
hxscyprodz <hxscyprodz@gmail.com>
2026-08-20 17:33:36 +0200

    Add the getApplication and approve application controllers

--- src/routes/applications/birthApp.routes.ts
+++ src/routes/applications/birthApp.routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import {
   createApplication,
   getApplications,
+  getApplication,
+  approveApplication,
 } from "../../controllers/applications/birthApp.controller";
 
 const router = Router();
 
 router.post("/apply", createApplication);
 router.get("/", getApplications);
+router.get("/:id", getApplication);
+router.patch("/:id/approve", approveApplication);
 
 export default router;
